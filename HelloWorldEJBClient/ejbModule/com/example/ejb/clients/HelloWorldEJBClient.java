e17ae9a6bbad4d4951f166e09c2b3100dcb45f10
emiliosmoreno <emiliosmoreno@gmail.com>
2014-11-27 16:36:29 +0100

    Limpieza

--- HelloWorldEJBClient/ejbModule/com/example/ejb/clients/HelloWorldEJBClient.java
+++ HelloWorldEJBClient/ejbModule/com/example/ejb/clients/HelloWorldEJBClient.java
@@ -7,16 +7,6 @@ import javax.naming.NamingException;
 
 import com.example.ejb.helloworld.HelloWorldEJBRemote;
 
-//app-name/module-name/bean-name!bean-interface
-//
-//where,
-//
-//app-name = the name of the .ear (without the .ear suffix) or the application name configured via application.xml deployment descriptor. If the application isn't packaged in a .ear then there will be no app-name part to the JNDI string.
-//module-name = the name of the .jar or .war (without the .jar/.war suffix) in which the bean is deployed or the module-name configured in web.xml/ejb-jar.xml of the deployment. The module name is mandatory part in the JNDI string.
-//bean-name = the name of the bean which by default is the simple name of the bean implementation class. Of course it can be overridden either by using the "name" attribute of the bean definining annotation (@Stateless(name="blah") in this case) or even the ejb-jar.xml deployment descriptor.
-//bean-interface = the fully qualified class name of the interface being exposed by the bean.
-//
-//So in our example above, l
 public class HelloWorldEJBClient {
 
 	
@@ -33,20 +23,6 @@ public class HelloWorldEJBClient {
         
     }
 	 
-
-		/*
-		 * 
-		java:global/HelloWorldEAR/HelloWorldEJB/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBLocal
-		java:app/HelloWorldEJB/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBLocal
-		java:module/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBLocal
-		java:global/HelloWorldEAR/HelloWorldEJB/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBRemote
-		java:app/HelloWorldEJB/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBRemote
-		java:module/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBRemote
-		java:jboss/exported/HelloWorldEAR/HelloWorldEJB/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBRemote
-		
-		*/
-//		String pathJNDI="java:module/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBRemote";
-	    
 	    
 	private static HelloWorldEJBRemote lookupRemote() throws NamingException {
 
@@ -63,10 +39,8 @@ public class HelloWorldEJBClient {
 			Context context = new InitialContext(propiedades);
 
 			String pathJNDI = "HelloWorldEAR/HelloWorldEJB/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBRemote";
-//			String pathJNDI = "java:/HelloWorldEAR/HelloWorldEJB/HelloWorldBean!com.example.ejb.helloworld.HelloWorldEJBRemote";
 			
 			miEJB = (HelloWorldEJBRemote) context.lookup(pathJNDI);
-//			System.out.println(miEJB.sayHello());
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
